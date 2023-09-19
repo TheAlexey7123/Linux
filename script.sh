3c5f02081ebf6625189df5254eaa2bc9232de6da
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2023-09-20 01:35:03 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+sudo apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+sudo apt install -y make cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
+sudo apt install -y apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
+
+cd Descargas/Linux
+cp picom.conf ~/.config/picom/picom.conf
+mkdir ~/.config/bspwm
+mkdir ~/.config/bspwm/scripts
+cp bspwm_resize.txt ~/.config/bspwm/scripts/bspwm_resize
+mkdir ~/.config/sxhkd
+cp sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp bspwmrc ~/.config/bspwm/bspwmrc
+mkdir ~/.config/kitty
+cp color.ini kitty.conf ~/.config/kitty
+https://www.gnome-look.org/p/https%3A%2F%2Fgithub.com%2Fshvchk%2Fpoly-dark%2Farchive%2Fmaster.zip
