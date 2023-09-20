2712372797cb12bed903235abe0e2c4775799b64
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2023-09-20 23:42:01 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -131,4 +131,10 @@ sudo cp * /usr/share/fonts/truetype
 fc-cache -v
 rm -rf ~/Documentos/blue-sky
 
+#p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+zsh
+sleep 5
+
 
