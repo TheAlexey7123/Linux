d10e748e39fe25eb5f241d2354a78f8d12cc4f88
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2023-09-20 21:24:54 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,13 +4,26 @@
 sudo apt install -y libxinerama1 libxinerama-dev build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
 sudo apt install -y make cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
 sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
+sudo apt install -y zsh-autosuggestions zsh-syntax-highlighting
+
+#plugin zsh autocomplete
+cd ~/Descargas
+mkdir /usr/share/zsh-autocomplete/
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+cd zsh-autocomplete/
+cp zsh-autocomplete.plugin.zsh /usr/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+sleep 2
+cd ..
+rm -rf zsh-autocomplete
 
 #Download programs
-sudo apt install -y 7zip bspwm sxhkd rofi picom
+sudo apt install -y 7zip bspwm sxhkd rofi picom feh
 
 #Update and upgrade
 sudo apt update
 sudo apt upgrade
+
+#kitty updated
 
 #picom:
 #cd ~/Descargas
