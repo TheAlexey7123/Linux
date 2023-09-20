0cfe64a7a8d0fb82da2efdfeb4464284c1a53e20
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2023-09-20 21:55:19 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,7 +20,7 @@ rm -rf zsh-autocomplete
 https://wallpapercave.com/wp/wp8148899.png
 
 #Download programs
-sudo apt install -y 7zip bspwm sxhkd rofi picom feh ranger
+sudo apt install -y 7zip bspwm sxhkd rofi picom feh ranger imagemagick
 
 #Update and upgrade
 sudo apt update
