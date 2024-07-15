ee8daadd362caa13b14dd33805e0cb44e4596eb6
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2024-07-15 13:28:05 +0200

    Update installDistro.sh

--- installDistro.sh
+++ installDistro.sh
@@ -37,12 +37,10 @@ sudo chmod +x /usr/local/bin/docker-compose
 #Polybar:
 #sudo apt install cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev -y
 git clone --recursive https://github.com/polybar/polybar
-mv polybar/* .
-sudo rm -r polybar/
-mkdir /home/alexey/Descargas/polybar/build
+mkdir /home/alexey/Descargas/Linux/polybar/build
 cmake .
 make -j$(nproc)
-sudo make install /home/alexey/Descargas/polybar/build
+sudo make install /home/alexey/Descargas//Linux/polybar/build
 
 #Picom:
 sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
