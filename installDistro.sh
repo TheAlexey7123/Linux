c91c384165b8fcb119888d31746de9107613ab6e
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2024-07-15 13:30:10 +0200

    Update installDistro.sh

--- installDistro.sh
+++ installDistro.sh
@@ -43,16 +43,16 @@ make -j$(nproc)
 sudo make install /home/alexey/Descargas//Linux/polybar/build
 
 #Picom:
-sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
-git clone https://github.com/ibhagwan/picom.git /home/alexey/Descargas/
+sudo apt install picom meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
+#git clone https://github.com/ibhagwan/picom.git /home/alexey/Descargas/
 
 mkdir /home/alexey/.config/picom/
 sudo mkdir /home/root/.config/picom/
 
-git submodule update --init --recursive /home/alexey/Descargas/picom/
-meson --buildtype=release /home/alexey/Descargas/picom/ build 
-ninja -C build /home/alexey/Descargas/picom/
-sudo ninja -C build install /home/alexey/Descargas/picom/
+#git submodule update --init --recursive /home/alexey/Descargas/picom/
+#meson --buildtype=release /home/alexey/Descargas/picom/ build 
+#ninja -C build /home/alexey/Descargas/picom/
+#sudo ninja -C build install /home/alexey/Descargas/picom/
 
 cp /home/alexey/Descargas/Linux/picom.conf /home/alexey/.config/picom/picom.conf 
 sudo cp /home/alexey/Descargas/Linux/picom.conf /home/alexey/.config/picom/picom.conf 
