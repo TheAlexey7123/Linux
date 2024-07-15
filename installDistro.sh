4083b82feeb04d4d613925ce868f1f51e998e17a
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2024-07-15 12:39:30 +0200

    Update installDistro.sh

--- installDistro.sh
+++ installDistro.sh
@@ -11,8 +11,9 @@ sudo apt-get update -y
 
 sudo apt install zip nmap net-tools libuv1-dev curl gobuster wfuzz burpsuite build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y
 sudo apt install dolphin libxinerama1 zsh libxinerama-dev bspwm -y
+sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
+sudo apt install acpi wmname scrub meson caja tmux xclip libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
 
-   
 
 git clone https://github.com/baskerville/bspwm.git /home/alexey/Descargas/
 git clone https://github.com/baskerville/sxhkd.git /home/alexey/Descargas/
