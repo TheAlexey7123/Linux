f72e5d540477177945e9c25a7b04241f16af16dd
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2024-07-21 18:47:01 +0200

    Update installDistro.sh

--- installDistro.sh
+++ installDistro.sh
@@ -19,7 +19,7 @@
 #sudo apt-get update -y
 #sudo setxkbmap es
 
-sudo apt-get install build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre2-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils expect
+sudo apt-get install -y build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre2-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils expect
 sudo apt install openvpn apache2 ftp smbclient aircrack-ng sshpass masscan jq nodejs npm arp-scan xinput pavucontrol php python3 nano gparted thunar lxappearance neofetch htop zip tree nmap x11-xserver-utils net-tools libuv1-dev curl gobuster wfuzz build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y
 sudo apt install dolphin libxinerama1 zsh libxinerama-dev bspwm -y
 sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
