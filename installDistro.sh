9b6e7437bea1d5ee46d3398cdad6b9ffdea15800
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2024-08-07 19:14:48 +0200

    Update installDistro.sh

--- installDistro.sh
+++ installDistro.sh
@@ -31,6 +31,7 @@ sudo apt install curl git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libr
 #sudo snap install exploitdb-bin-sploits
 sudo apt install gpgv2 autoconf bison build-essential postgresql libaprutil1 libgmp3-dev libpcap-dev openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool libxml2 libxml2-dev libxslt-dev wget libyaml-dev ncurses-dev  postgresql-contrib xsel zlib1g zlib1g-dev curl -y
 sudo gem install wpscan
+sudo apt install mariadb-server -y
 
 #Instalar metasploit:
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
