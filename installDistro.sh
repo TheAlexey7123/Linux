2756a27d57192f9713e70306f7717b74ad30491e
TheAlexey7123 <71777836+TheAlexey7123@users.noreply.github.com>
2024-08-08 02:56:59 +0200

    Update installDistro.sh

--- installDistro.sh
+++ installDistro.sh
@@ -25,7 +25,7 @@ sudo apt install openvpn apache2 whatweb exiftool ftp smbclient aircrack-ng sshp
 sudo apt install dolphin libxinerama1 hydra zsh libxinerama-dev bspwm -y
 sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
 sudo apt install acpi wmname scrub meson caja tmux xclip libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev -y
-sudo apt install curl alsa-utils brightnessctl git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config ruby ruby-bundler ruby-dev -y
+sudo apt install curl alsa-utils brightnessctl flameshot git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config ruby ruby-bundler ruby-dev -y
 #sudo snap install searchsploit
 #sudo snap install exploitdb-papers
 #sudo snap install exploitdb-bin-sploits
